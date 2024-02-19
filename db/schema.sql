e2383603448040af5f946c607c93b5af9df7d2ca
Jacob kaufenberg <Jacob.kaufenberg@icloud.com>
2024-02-18 21:03:56 -0600

    installed npm and started db tables

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS employee_db;
+
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY;
+    first_name VARCHAR(30) NOT NULL;
+    last_name VARCHAR(40) NOT NULL;
+    title 
+
+ )
